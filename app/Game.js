ca935f932c91541d7f01e490bda502c8369f6af7
Grochowsky <grzesiugg@gmail.com>
2022-07-19 11:42:14 +0200

    Game development is over - next-  add API

--- app/Game.js
+++ app/Game.js
@@ -1,4 +1,14 @@
+import { Quote } from './Quote.js'
+
 class Game {
+
+  currentStep = 0;
+  lastStep = 7;
+
+  quotes = [{
+    text: 'pan tadeusz',
+    category: 'Utwór Literacki'
+  }]
   constructor({
     lettersWrapper,
     categoryWrapper,
@@ -9,14 +19,55 @@ class Game {
     this.categoryWrapper = categoryWrapper
     this.wordWrapper = wordWrapper
     this.outputWrapper = outputWrapper
+
+    const {text, category} = this.quotes[Math.floor(Math.random() * this.quotes.length)]
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text);
+    
   }
-  start() {
+  guess(letter, event){
+    event.target.disabled = true;
+    if(this.quote.guess(letter)){
+      this.drawQuote()
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+      if(this.currentStep == this.lastStep ){
+        this.loosing();
+      }
+    }
+  }
+  drawLetters(){
     for(let i = 0; i<26; i++){
       const label = (i+10).toString(36);
       const button = document.createElement('button');
       button.innerHTML = label;
+      button.addEventListener('click', (event) => {
+        this.guess(label,event)
+      })
       this.lettersWrapper.appendChild(button);
     }
+  }
+
+  drawQuote(){
+    const content = this.quote.getContent();
+    this.wordWrapper.innerHTML = content;
+    if (!content.includes('_')){
+      this.winning()
+    }
+  }
+  start() {
+    document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+    this.drawLetters();
+    this.drawQuote();
+  }
+  winning(){
+    this.wordWrapper.innerHTML = 'GRATULACJE! WYGRAŁEŚ! KONIEC GRY';
+    this.lettersWrapper.innerHTML = ' ';
+  }
+  loosing(){
+    this.wordWrapper.innerHTML = 'NIESTETY PRZEGRAŁEŚ! SPRÓBUJ JESZCZE RAZ!';
+    this.lettersWrapper.innerHTML = ' ';
   }
 }
 
